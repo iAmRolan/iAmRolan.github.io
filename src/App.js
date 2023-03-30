26a6daecf5ed7c56d1b1d8044eee0dd6b7b73249
Rolan Abdualiev <82762541+iAmRolan@users.noreply.github.com>
2023-03-30 16:06:46 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from './components/About';
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Work/>
+    </div>
+  );
+}
+
+export default App;
